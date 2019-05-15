28d03eba5449f5fcbdf724bb8de48a3508fe2833
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-15 11:13:05 -0700

    added all the stack opcode functions

---
+++ opcodes.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *param = strtok(NULL, " \n");
+	int n;
+
+	if (check_string(param) == -1)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(param);
+
+	*stack = add_dnodeint(stack, n);
+}
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	delete_dnodeint_at_index(stack, 0);
+}
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+		(void)line_number;
+		print_dlistint(*stack);
+}
