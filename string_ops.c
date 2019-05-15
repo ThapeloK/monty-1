7386414b99820d3260baa3e37790c0bd5bb73354
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-15 11:14:27 -0700

    added all string related functions

---
+++ string_ops.c
@@ -0,0 +1,60 @@
+#include "monty.h"
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+
+int check_string(char *s)
+{
+	int i = 0;
+
+	while(s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			i++;
+		}
+		else
+			return (-1);
+	}
+	return (1);
+}
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	(void) line_number;
+	if (!*stack)
+	{
+		printf("\n");
+	}
+	else if (temp || (temp->n >= 32 && temp->n > 126) || temp->n)
+	{
+		printf("%c", temp->n);
+		temp = temp->next;
+	}
+}
+
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 32 || (*stack)->n > 126)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*stack)->n);
+}
